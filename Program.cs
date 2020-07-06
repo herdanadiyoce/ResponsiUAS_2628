9aed9345c7f34bec33881fb240ba93c3ab4991c1
herdanadiyoce <61857321+herdanadiyoce@users.noreply.github.com>
2020-07-06 18:00:42 +0700

    Responsi Pemrograman UAS 19.11.2628

---
+++ Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectProduk
+{
+    class Program
+    {
+        // deklarasi objek collection untuk menampung objek produk
+        static List<Produk> daftarProduk = new List<Produk>();
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Responsi UAS Matakuliah Pemrograman";
+
+            while (true)
+            {
+                TampilMenu();
+
+                Console.Write("\nNomor Menu [1..4]: ");
+                int nomorMenu = Convert.ToInt32(Console.ReadLine());
+
+                switch (nomorMenu)
+                {
+                    case 1:
+                        TambahProduk();
+                        break;
+
+                    case 2:
+                        HapusProduk();
+                        break;
+
+                    case 3:
+                        TampilProduk();
+                        break;
+
+                    case 4: // Keluar Dari Program
+                        return;
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        static void TampilMenu()
+        {
+            Console.Clear();
+
+            // PERINTAH: lengkapi kode untuk menampilkan menu
+            Console.Clear();
+            Console.WriteLine("Pilih Menu Aplikasi");
+            Console.WriteLine();
+            Console.WriteLine("1. Tambah Produk");
+            Console.WriteLine("2. Hapus Produk");
+            Console.WriteLine("3. Tampilkan Produk");
+            Console.WriteLine("4. Keluar");
+        }
+
+        static void TambahProduk()
+        {
+            Console.Clear();
+
+            // PERINTAH: lengkapi kode untuk menambahkan produk ke dalam collection
+            Console.WriteLine();
+            Console.WriteLine("Tambah Data Produk\n");
+            Produk produk = new Produk();
+            Console.Write("Kode Produk : ");
+            produk.kodeproduk = Console.ReadLine();
+            Console.Write("Nama Produk : ");
+            produk.namaproduk = Console.ReadLine();
+            Console.Write("Harga Beli  : ");
+            produk.hargabeli = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Harga Jual  : ");
+            produk.hargajual = Convert.ToDouble(Console.ReadLine());
+            daftarProduk.Add(produk);
+            Console.WriteLine();
+            Console.WriteLine("\nTekan ENTER untuk kembali ke menu");
+            Console.ReadKey();
+        }
+
+        static void HapusProduk()
+        {
+            Console.Clear();
+
+            // PERINTAH: lengkapi kode untuk menghapus produk dari dalam collection
+
+            int no = -1, hapus = -1;
+            Console.WriteLine("Hapus Data Produk\n");
+            Console.Write("Kode Produk : ");
+            string kode = Console.ReadLine();
+            foreach (Produk produk in daftarProduk)
+            {
+                no++;
+                if (produk.kodeproduk == kode)
+                {
+                    hapus = no;
+                }
+            }
+            if (hapus != -1)
+            {
+                daftarProduk.RemoveAt(hapus);
+                Console.WriteLine("\nData Produk Berhasil dihapus");
+            }
+            else
+            {
+                Console.WriteLine("\nKode Produk tidak ditemukan");
+            }
+
+            Console.WriteLine("\nTekan ENTER untuk kembali ke menu");
+            Console.ReadKey();
+        }
+
+        static void TampilProduk()
+        {
+            Console.Clear();
+
+            // PERINTAH: lengkapi kode untuk menampilkan daftar produk yang ada di dalam collection
+
+            int noUrut = 0;
+            Console.WriteLine("Data Produk\n");
+            foreach (Produk produk in daftarProduk)
+            {
+                noUrut++;
+                Console.WriteLine("{0}. {1}, {2}, {3:N0}, {4:N0}", noUrut, produk.kodeproduk, produk.namaproduk, produk.hargabeli, produk.hargajual);
+            }
+            if (noUrut < 1)
+            {
+                Console.WriteLine("Data Produk Kosong");
+            }
+
+            Console.WriteLine("\nTekan enter untuk kembali ke menu");
+            Console.ReadKey();
+        }
+    }
+}
